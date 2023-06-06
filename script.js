7ebb431b9f19c61ef2ffcc4e9e9a99a2f8f22e0b
flori <sf.simion.f@gmail.com>
2023-06-06 11:25:55 +0200

    solved introductory exercices

---
+++ script.js
@@ -0,0 +1,16 @@
+//////// printing the document title in the console.log//////
+console.log(document.title) /// expected answer :"Alchemy for beginers"
+
+//////// changing the document title /////
+
+document.title="Modifying the DOM" 
+console.log(document.title) ///// expected answer : " Modifying the DOM"
+
+
+/////Changing the background color of the body to hot pink (#FF69B4)//////////
+document.body.style.backgroundColor="#FF69B4"  
+
+/////////Changing the background color of the body to a random color///////////
+document.body.style.backgroundColor="#" + Math.floor(0xffffff * Math.random())
+
+/////////Displaying the children of body//////

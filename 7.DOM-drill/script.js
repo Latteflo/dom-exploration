533c7f6dd4d1269bb968860ee5ab60c70456192c
flori <sf.simion.f@gmail.com>
2023-06-08 16:04:27 +0200

    added solution for the last task

--- 7.DOM-drill/script.js
+++ 7.DOM-drill/script.js
@@ -28,7 +28,7 @@ let children = ul.childNodes
 
 // Function to handle the click event -alerts the name of the clicked movie with the exception of FF
 function handleClick(e) {
-  // If 'Fast and Furious' is clicked, display the special message Diogo wants
+  // If FF is clicked, display the special message Diogo wants.
   if (e.target.textContent.includes("Fast and Furious")) {
     alert(
       "The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family."
@@ -37,6 +37,7 @@ function handleClick(e) {
     alert(e.target.textContent) // else, display the name of the clicked item
   }
 }
+
 // Iterate over every child
 for (let i = 0; i < children.length; i++) {
   // Check if the node is an element node
@@ -44,7 +45,7 @@ for (let i = 0; i < children.length; i++) {
     // Add the click event listener
     children[i].addEventListener("click", handleClick)
 
-    // Check if the element contains 'Fast and Furious'
+    // Check if the element contains FF
     if (children[i].textContent.includes("Fast and Furious")) {
       ul.insertBefore(children[i], ul.firstChild) // Move it to the top of the list
       children[0].classList.add("important") // Add the 'important' class to it
@@ -57,49 +58,66 @@ document.body.addEventListener("keypress", (e) => {
   if (e.code === "R") {
     // Create a new array with the children of the ul
     let array = Array.from(ul.children)
-    let important = array.splice(  
+    let important = array.splice(
       array.findIndex((item) => item.textContent.includes("Fast and Furious")),
       1
-    ) // remove the 'Fast and Furious' item
+    ) // remove the FF item
     array.sort(() => Math.random() - 0.5) //shuffle the rest
-    array.unshift(important[0]) //add 'Fast and Furious' back at the start
+    array.unshift(important[0]) //add FF back at the start
     // Append each item in the new order
     array.forEach((item) => ul.appendChild(item))
   }
   if (e.code === "D") {
-    // Clone 'Fast and Furious' item
+    // Clone FF item
     let clone = document.querySelector(".important").cloneNode(true)
     ul.appendChild(clone) //// append it to the ul
   }
 })
 
-//Using createElement, create a <select> tag into the previously created <div>, 
-//with two <option>s in it: "important franchises" and "normal franchises"
+// Add the functional select with options tags on change- .important - visible//
 
-//let selectorContainer = document.createElement("div") /// created the div
-//document.body.appendChild(selectorContainer)
-//let selector= document.createElement("select")// created the selector
-//let option1=document.createElement('option').textContent="important francises" //option 1
-//let option2= document.createElement('option').textContent="normal francises"///option 2
-//selectorContainer.appendChild("select")
-//selector.appendChild('option1')
-//selector.appendChild('option2') // we append the 2 options
-// // put the selector inside the div
+// we create the div
+let selectorContainer = document.createElement("div")
+// we create the select tag
+let selector = document.createElement("select")
+//we declare the first option
+let option1 = document.createElement("option")
+// we add the text to the option
+option1.textContent = "important franchises"
+// we add the value to the option
+option1.value = "0"
+// we declare the second option
+let option2 = document.createElement("option")
+// we add text to the second option
+option2.textContent = "normal franchises"
+// we add the value to the option
+option2.value = "1"
 
+// we append the children to the parent selector
+selector.appendChild(option1)
+selector.appendChild(option2)
+// we append the selector the the created div
+selectorContainer.appendChild(selector)
 
-//selector.addEventListener("change", handleChange)   
+// we append the div to the body
+document.body.appendChild(selectorContainer)
+// we declare a handleChange that takes care of the interraction
 
-//const handleChange =(change)=>{
-//    switch (change) {
-//        case '0':
-//            console.log(option1)
-//            break;   
-//        case '1':
-//            console.log(option2)
-//            break;
-//    } 
-//}
+const handleChange = (event) => {
+  // we target the value of the options
+  let selectedValue = event.target.value
+  //we add the condition - which is if the first option is selected
+  if (selectedValue === "0") {
+    // we select the items with the class of important
+    let items = ul.querySelectorAll(".important")
+    // we add the style vizibility to the important items
+    items.forEach((item) => (item.style.visibility = "visible"))
+    //we target the items that do NOT have the class of important
+    let notImportantItems = ul.querySelectorAll(":not(.important)")
+    // we adjust the visibility to hidden for these
+    notImportantItems.forEach((item) => (item.style.visibility = "hidden"))
+  }
+}
 
-//Add an eventListener to the <select>, on change,
-// if the option "important franchise" is chosen, only display items of the list that have the class .important.
-//  (hint: you can toggle visibility using element.style.visibility = 'hidden')*/
+// we add the event listener that will check which option is clicked and toggle the handleChange function
+selector.addEventListener("change", handleChange)
